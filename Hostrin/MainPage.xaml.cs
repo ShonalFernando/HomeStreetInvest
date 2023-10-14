026c96bcb21d369120413b9b09120fef78352fdd
Mihindukulasuriya Shonal Nirash Fernando <129303512+ShonalFernando@users.noreply.github.com>
2023-10-14 16:47:50 +0530

    Initial Commit

---
+++ Hostrin/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Hostrin;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Hostrin/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Hostrin;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
