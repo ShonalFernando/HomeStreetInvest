c9dea3f6a8b2bfa74851e1684381006826c28b67
Mihindukulasuriya Shonal Nirash Fernando <129303512+ShonalFernando@users.noreply.github.com>
2023-11-07 21:38:32 +0530

    Major UI Changes

---
+++ Homsin/Session/SessionDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homsin.Session
+{
+    public static class SessionDAO
+    {
+        public static string MSZoning;
+        public static string LotArea;
+        public static string LotConfig;
+        public static string BldgType;
+        public static string OverallCond;
+        public static string YearBuilt;
+        public static string YearRemodAdd;
+        public static string Exterior1st;
+        public static string BsmtFinSF2;
+        public static string TotalBsmtSF;
+        public static string MSSubClass;
+
+    }
+}
